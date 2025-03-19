404042efca9ad6782ec791b1c5d33594e31b1b0f
akashhandi1 <akashhandi1@gmail.com>
2025-03-19 16:22:23 +0530

    Added project files

--- src/recurr/fibo.java
+++ src/recurr/fibo.java
@@ -5,14 +5,19 @@ import java.util.Stack;
 
 public class fibo {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        System.out.println(fib(n));
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+        System.out.println(fibonaccino(50));
     }
-    static int  fib(int n){
-        if(n<2){
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
+
+
+    static int fibonaccino(int n){
+        return (int)((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
     }
+//    static int  fib(int n){
+//        if(n<2){
+//            return n;
+//        }
+//        return fib(n-1)+fib(n-2);
+//    }
 }
