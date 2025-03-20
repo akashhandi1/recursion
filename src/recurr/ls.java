649d29d36179725e771f06048f1ab1c7f4cd4c76
akashhandi1 <akashhandi1@gmail.com>
2025-03-20 17:15:35 +0530

    20.3.2025 2nd

--- src/recurr/ls.java
+++ src/recurr/ls.java
@@ -1,11 +1,15 @@
 package recurr;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 
 public class ls {
     public static void main(String[] args) {
-        int[] arr= {2,5,8,9,12,48,56,78,2,45};
-        System.out.println(lis(arr,49,0));
+        int[] arr= {2,5,8,9,12,48,56,78,2,45,48};
+        System.out.println(lis(arr,48,0));
+        System.out.println(findindex(arr,48,0));
+        ArrayList<Integer> ans = findallindex(arr,48,0,new ArrayList<>());
+        System.out.println(ans);
     }
 
     static boolean lis(int[] arr, int target,int index){
@@ -14,5 +18,39 @@ public class ls {
         }
 
         return arr[index]==target || lis(arr,target,index+1);
+    }
+
+    static int findindex(int[] arr, int target, int index){
+        if(index == arr.length){
+            return -1;
+        }
+        if(arr[index] == target){
+            return index;
+        }
+
+        return findindex(arr,target,index+1);
+    }
+
+//    static ArrayList<Integer>  list = new ArrayList<>();
+//    static void findallindex(int[] arr, int target, int index) {
+//        if (index == arr.length ) {
+//            return;
+//        }
+//        if (arr[index] == target) {
+//            list.add(index);
+//        }
+//        findallindex(arr, target, index + 1);
+//
+//    }
+
+    static ArrayList<Integer> findallindex(int[] arr,int target,int index, ArrayList<Integer> list){
+        if(index == arr.length){
+            return list;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+
+        return findallindex(arr, target, index+1, list);
     }
 }
