3d04d17f59552bbe35d828769700fc19a174dbc4
akashhandi1 <akashhandi1@gmail.com>
2025-04-03 16:16:29 +0530

    march3(1)

--- src/sorting/mergesort.java
+++ src/sorting/mergesort.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 public class mergesort {
     public static void main(String[] args) {
         int[] arr = {5,4,3,2,1};
-        arr = mergeSort(arr);
+        mergeSortInPlace(arr, 0, arr.length);
         System.out.println(Arrays.toString(arr));
     }
-
+    // HERE in mergesort it uses left and right arrays but in inplace it uses  mix array, but in inplace O(n)
+    // O(1) space complexity-it edits original array w/o using extra array
     static int[] mergeSort(int[] arr){
         if(arr.length ==1 ){
             return arr;
@@ -53,5 +54,54 @@ public class mergesort {
 
         return mix;
 
+    }
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+
+        int mid = (s + e) / 2;
+
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s+l] = mix[l];
+        }
     }
 }
